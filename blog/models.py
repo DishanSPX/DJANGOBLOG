b504d19f57411f3455bb6cfb60819f00a36f40bd
DishanSPX <dishan077sanjaya@gmakil.com>
2026-09-14 15:33:40 +0900

    Update Project

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 class Post(models.Model):
     STATUS_CHOICES = [
@@ -11,5 +12,12 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
     def __str__(self):
         return self.title
+
+# Create your models here.
